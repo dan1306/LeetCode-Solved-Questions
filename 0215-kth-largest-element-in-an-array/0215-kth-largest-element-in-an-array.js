f27737aead4cfb6319dca5e0c3adff8be900bcfd
dan1306 <81488150+dan1306@users.noreply.github.com>
2022-10-27 09:58:11 -0400

    Time: 340 ms (23.53%), Space: 63.6 MB (9.65%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    
+    
+    let arr = sortArray(nums).reverse()
+    
+    return arr[k - 1]
+    
+};
+
+
+
+function sortArray(nums){
+
+  if(nums.length == 1){
+    return nums
+  }
+
+  let mid = nums.length / 2
+  let subLeft = sortArray(nums.slice(0, mid))
+  let subRight = sortArray(nums.slice(mid))
+
+
+  return merge(subLeft, subRight)
+}
+
+function merge(subLeft, subRight){
+
+  let res = []
+  let r = 0
+  let l = 0
+
+  while( l < subLeft.length && r < subRight.length){
+    if(subLeft[l] < subRight[r]){
+      res.push(subLeft[l])
+      l++
+    } else{
+       res.push(subRight[r])
+      r++
+    }
+  }
+
+  return(res.concat(subLeft.slice(l)).concat(subRight.slice(r)))
+}

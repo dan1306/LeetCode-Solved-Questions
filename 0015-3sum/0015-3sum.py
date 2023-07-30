0d48c8078358cfbbdc788a6923d4d14f549b262e
Daniel Sakyi <81488150+dan1306@users.noreply.github.com>
2023-07-30 18:50:08 -0400

    Time: 5175 ms (11.91%), Space: 21 MB (14.85%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,37 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        soluObj = {}
+
+        nums.sort()
+
+        while len(nums) >= 3:
+
+            initial = 0
+            l = initial + 1
+            r = len(nums) - 1
+            while l != r:
+                ans = nums[initial] + nums[l] + nums[r]
+                ansArr = [nums[initial], nums[l], nums[r]]
+                ansArr.sort()
+
+
+                if ans == 0:
+                    key = f"{ansArr[0]}{ansArr[1]}{ansArr[2]}"
+
+                    if key not in soluObj:
+                        soluObj[key] = ansArr
+                    r-= 1
+                if ans > 0:
+                    r -= 1
+                if ans < 0:
+                    l += 1
+            nums.pop(0)
+        # print(soluObj)
+
+        arr = []
+
+        for i in soluObj:
+            arr.append(soluObj[i])
+
+        return arr

7ebb097e9362f7d9cbb5c9ef2848e4cff33c6bbf
Daniel Sakyi <81488150+dan1306@users.noreply.github.com>
2023-08-15 10:57:30 -0400

    Time: 16 ms (97.44%), Space: 13.6 MB (66.91%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,46 +1,33 @@
 class Solution(object):
     def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        arr = []
-        n = 0
+            """
+            :type matrix: List[List[int]]
+            :type target: int
+            :rtype: bool
+            """
+            for i in matrix:
 
-        for i in matrix:
-            
-            if i[0] <= target and i[-1] >= target:
                 if i[0] == target or i[-1] == target:
                     return True
-                l = 0
-                r = len(i) - 1
 
+                if i[0] <= target and i[-1] >= target:
 
-                print(i)
+                    l = 0
+                    r = len(i) - 1
 
-                while l <= r:
+                    while l <= r:
+                        mid = (l + r) // 2
 
-                    print(l, r)
+                        if i[mid] > target:
+                            r = mid - 1
+                            continue
 
-                    mid = (l + r) // 2
+                        if i[mid] < target:
+                            l = mid + 1
+                            continue
 
-                    if i[mid] > target:
-                        r = mid - 1
-                        continue
+                        if target is i[mid]:
+                            print(i[mid])
+                            return True
 
-                    if i[mid] < target:
-                        l = mid + 1
-                        continue
-
-                    if target is i[mid]:
-                        print(i[mid])
-                        return True
-
-                return False
-
-
-
-                break
-        # if len(arr) == 0:
-        return False
+                    return False

eff866b2de706715663680cf74acdccea42aff87
Daniel Sakyi <81488150+dan1306@users.noreply.github.com>
2025-05-05 20:41:46 -0400

    0026-REMOVE-DUPLICATES-FROM-SORTED-ARRAY Stats: Time: 259 ms (5%), Space: 13.3 MB (77.08%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -6,24 +6,22 @@ class Solution(object):
         """
         # for i in range(0, len())
 
-        if(len(nums) == 0):
+        if (len(nums) == 0):
             return 0;
-        if(len(nums) == 1):
+        if (len(nums) == 1):
             return 1;
-        
+
         k = 1;
         element_at_index = nums[0]
         curr = 1;
+        print("element at curr " + str(curr) + " " + str(nums[curr]));
+
         while(curr < len(nums)):
-            if(nums[curr] == element_at_index):
-                nums[curr] = " ";
-                curr+=1;
+            print("element at curr " + str(curr) + " " + str(nums[curr]));
+            if (nums[curr] == element_at_index):
+                print("popping " + str(nums[curr]))
+                nums.pop(curr);
             else:
                 element_at_index = nums[curr];
-                curr+=1;
-                k+=1;
-        # print("ans is = " + str(k) )
-        # return k
-        
-        while " " in nums:
-            nums.remove(" ");
+                curr += 1;
+                k += 1;

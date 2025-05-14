7af6305ff01ba69ab379fae2f8fdb5974a1a9f45
Daniel Sakyi <81488150+dan1306@users.noreply.github.com>
2025-05-14 18:13:28 -0400

    0021-MERGE-TWO-SORTED-LISTS Stats: Time: 3 ms (23.92%), Space: 12.5 MB (45.98%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,26 +1,66 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
-        
-        
-                
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if not list1 and not list2:
+            return ListNode().next
+        elif list1 and not list2:
+            return list1
+        elif list2 and not list1:
+            return list2
+                        
         dummy = ListNode()
         tail = dummy
-        
+        first  = 0
+
         while list1 and list2:
-            print(list1.val, list2.val)
-            if list1.val < list2.val:
-                tail.next = list1
+
+            if(list1.val > list2.val):
+                if(first == 0):
+                    tail.val = list2.val
+                    first = 1
+
+                else:
+                    dono = ListNode()
+                    dono.val = list2.val
+                    tail.next = dono
+                    tail = tail.next
+
+                list2 = list2.next
+            else:
+                if(first == 0):
+                        tail.val = list1.val
+                        first = 1
+
+                else:
+                    dono = ListNode()
+                    dono.val = list1.val
+                    tail.next = dono
+                    tail = tail.next
+                list1 = list1.next
+
+        if list1:
+            while list1:
+                dono = ListNode()
+                dono.val = list1.val
+                tail.next = dono
                 tail = tail.next
                 list1 = list1.next
-            else:
-                tail.next = list2
+        else:
+            while list2:
+                dono = ListNode()
+                dono.val = list2.val
+                tail.next = dono
                 tail = tail.next
                 list2 = list2.next
-            tail.next = None
-            
-        if list1 != None:
-            tail.next = list1
-        elif list2 != None:
-            tail.next = list2
-            
-        return dummy.next
+
+        return dummy
+        
--- README.md
+++ README.md
@@ -52,9 +52,11 @@ Collection of LeetCode questions to ace the coding interview! - Created using [L
 ## Linked List
 |  |
 | ------- |
+| [0021-merge-two-sorted-lists](https://github.com/dan1306/LeetCode-Solved-Questions/tree/master/0021-merge-two-sorted-lists) |
 | [0206-reverse-linked-list](https://github.com/dan1306/LeetCode-Solved-Questions/tree/master/0206-reverse-linked-list) |
 ## Recursion
 |  |
 | ------- |
+| [0021-merge-two-sorted-lists](https://github.com/dan1306/LeetCode-Solved-Questions/tree/master/0021-merge-two-sorted-lists) |
 | [0206-reverse-linked-list](https://github.com/dan1306/LeetCode-Solved-Questions/tree/master/0206-reverse-linked-list) |
 <!---LeetCode Topics End-->

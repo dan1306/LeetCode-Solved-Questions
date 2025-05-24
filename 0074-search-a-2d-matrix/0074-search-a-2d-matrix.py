9edfbfea1b0c6e04c7d44257904fe040f64f1f9c
Daniel Sakyi <81488150+dan1306@users.noreply.github.com>
2025-05-24 13:42:56 -0400

    0074-SEARCH-A-2D-MATRIX Stats: Time: 0 ms (100%), Space: 12.5 MB (90.95%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -9,48 +9,41 @@ class Solution(object):
         for  i in matrix:
 
             if i[0] <= target and i[len(i) - 1] >= target:
-                momentoftruth= search(i, target) 
+                momentoftruth= self.search(i, target) 
                 if momentoftruth is not -1:
                     return True
                 
         return False
-        
+
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        first = 0
+        second = len(nums) - 1
 
 
+        while first < second and first is not second:
+            mid = (first + second) / 2
 
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                second = mid
+            else:
+                first = mid 
 
+            if first + 1 == second :
+                break
 
-
-
-def search(nums, target):
-    """
-    :type nums: List[int]
-    :type target: int
-    :rtype: int
-    """
-
-    first = 0
-    second = len(nums) - 1
-
-
-    while first < second and first is not second:
-        mid = (first + second) / 2
-
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] > target:
-            second = mid
-        else:
-            first = mid 
-
-        if first + 1 == second :
-            break
-
-    if nums[first] == target:
-        return first 
-    elif nums[second] == target:
-        return second 
-    return -1
+        if nums[first] == target:
+            return first 
+        elif nums[second] == target:
+            return second 
+        return -1
 
     
     

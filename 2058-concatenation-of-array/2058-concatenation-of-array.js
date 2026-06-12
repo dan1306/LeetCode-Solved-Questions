60b64e683b8912ed79e2956d345f02c28833ad1d
Daniel Sakyi <81488150+dan1306@users.noreply.github.com>
2026-06-12 10:38:21 -0400

    2058-CONCATENATION-OF-ARRAY Stats: Time: 2 ms (32.29%), Space: 58.5 MB (44.18%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.js
+++ 2058-concatenation-of-array/2058-concatenation-of-array.js
@@ -6,19 +6,29 @@ var getConcatenation = function(nums) {
 
 
     // let numberOfIncrementOverTheWholeNumsArray =0;
-    // let index = 0;
-    let ans = [...nums, ...nums];
+    // // let index = 0;
+    // let ans = [...nums, ...nums];
 
 
-    // while(numberOfIncrementOverTheWholeNumsArray < 2 ){
+    // // while(numberOfIncrementOverTheWholeNumsArray < 2 ){
 
-    //     ans.push(nums[index])
+    // //     ans.push(nums[index])
 
-    //     index+=1;
-    //     if(index === nums.length){
-    //         index = 0;
-    //         numberOfIncrementOverTheWholeNumsArray+= 1;
-    //     }
-    // }
+    // //     index+=1;
+    // //     if(index === nums.length){
+    // //         index = 0;
+    // //         numberOfIncrementOverTheWholeNumsArray+= 1;
+    // //     }
+    // // }
+    // return ans;
+
+    let ans = [];
+
+    for(let i = 0; i < 2; i++){
+        for(let i = 0; i < nums.length; i++){
+            ans.push(nums[i]);
+        }
+    }
+
     return ans;
 };

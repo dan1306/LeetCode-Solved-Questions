ad04eb201dd25a0dc1c317b7634802a145d58b8e
Daniel Sakyi <81488150+dan1306@users.noreply.github.com>
2026-06-13 23:18:08 -0400

    0206-REVERSE-LINKED-LIST Stats: Time: 10 ms (0.88%), Space: 56.6 MB (83.31%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+
+    if(!head){
+        return head;
+    }
+
+    if(!head.next){
+        return head;
+    }
+    
+    let pre = null;
+    let curr = head;
+    let next = curr.next;
+
+    while(curr.next){
+       curr.next = pre;
+       pre = curr
+       curr = next;
+       next = next.next;
+    //    next = next.next;
+    }
+
+    curr.next = pre
+
+    console.log(pre)
+    return curr;
+
+
+    
+};

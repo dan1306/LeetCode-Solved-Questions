95db41a3dcfacfbfbca02cdbfc376692dbf67f06
Daniel Sakyi <81488150+dan1306@users.noreply.github.com>
2026-06-16 22:27:35 -0400

    1231-REPLACE-ELEMENTS-WITH-GREATEST-ELEMENT-ON-RIGHT-SIDE Stats: Time: 36 ms (13.13%), Space: 65.5 MB (6.91%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.js
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.js
@@ -3,8 +3,6 @@
  * @return {number[]}
  */
 
-
-
 var replaceElements = function(arr) {
 
     if(arr.length === 0) return arr;
@@ -14,31 +12,65 @@ var replaceElements = function(arr) {
     }
 
     let dummy = []
-    let maxElemement = null;
-    let indexOfMaElement = null;
-    let whereWeareAtinArr = 0;
-    while(dummy.length < arr.length - 1 ){
+    let obj = {}
+    let ittBack = arr.length - 2;
+    while(ittBack >= 0){
 
-        if((maxElemement && indexOfMaElement) && indexOfMaElement > whereWeareAtinArr){
-            dummy.push(maxElemement)
-        } else{
-            maxElemement = null;
-            indexOfMaElement = null;
-            for(let i = whereWeareAtinArr + 1; i < arr.length; i++){
-                if(arr[i] > maxElemement || !maxElemement){
-                    indexOfMaElement = i;
-                    maxElemement = arr[i];
-                }
+        if(ittBack === arr.length -2){
+            obj[ittBack] = arr[ittBack + 1]
+        }else{
+            if(arr[ittBack + 1] > obj[ittBack + 1]){
+                obj[ittBack] = arr[ittBack + 1];
+            } else{
+                obj[ittBack] = obj[ittBack + 1];
             }
-            dummy.push(maxElemement);
         }
-        whereWeareAtinArr+=1;
-
+        ittBack -= 1;
     }
-    dummy.push(-1)
-    return dummy;
 
+    for(let i = 0; i < arr.length -1; i++){
+        dummy.push(obj[i]);
+    }
+
+    dummy.push(-1);
+    return dummy;
 }
+
+
+// var replaceElements = function(arr) {
+
+//     if(arr.length === 0) return arr;
+//     if(arr.length === 1){
+//         arr[0] = -1;
+//         return arr;
+//     }
+
+//     let dummy = []
+//     let maxElemement = null;
+//     let indexOfMaElement = null;
+//     let whereWeareAtinArr = 0;
+//     while(dummy.length < arr.length - 1 ){
+
+//         if((maxElemement && indexOfMaElement) && indexOfMaElement > whereWeareAtinArr){
+//             dummy.push(maxElemement)
+//         } else{
+//             maxElemement = null;
+//             indexOfMaElement = null;
+//             for(let i = whereWeareAtinArr + 1; i < arr.length; i++){
+//                 if(arr[i] > maxElemement || !maxElemement){
+//                     indexOfMaElement = i;
+//                     maxElemement = arr[i];
+//                 }
+//             }
+//             dummy.push(maxElemement);
+//         }
+//         whereWeareAtinArr+=1;
+
+//     }
+//     dummy.push(-1)
+//     return dummy;
+
+// }
 
 // var replaceElements = function(arr) {
 

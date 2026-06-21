6f8a8137c54f028b871456b983bb70d60d8b1193
Daniel Sakyi <81488150+dan1306@users.noreply.github.com>
2026-06-21 09:35:40 -0400

    0021-MERGE-TWO-SORTED-LISTS Stats: Time: 2 ms (12.34%), Space: 58.6 MB (7.66%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    let dummyNode  = new ListNode();
+    let head = dummyNode;
+    while(list1 && list2){
+
+        if(list1.val <= list2.val){
+            let nextInput = new ListNode(list1.val);
+            head.next = nextInput;
+            head = head.next;
+            list1 = list1.next;
+        }else{
+            let nextInput = new ListNode(list2.val);
+            head.next = nextInput;
+            head = head.next;
+            list2 = list2.next;
+        }
+    }
+    if(list1){
+            head.next = list1;
+            // head = head.next;
+            // list2 = list2.next;
+    } else if(list2){
+            head.next = list2;
+
+    }
+
+    return dummyNode.next;
+};

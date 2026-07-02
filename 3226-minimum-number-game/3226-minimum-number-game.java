5d79a2639a8e76da196a2552c03f19693b0513a4
Daniel Sakyi <81488150+dan1306@users.noreply.github.com>
2026-07-02 12:17:51 -0400

    3226-MINIMUM-NUMBER-GAME Stats: Time: 1 ms (100%), Space: 46.9 MB (70.94%) - LeetHub

--- 3226-minimum-number-game/3226-minimum-number-game.java
+++ 3226-minimum-number-game/3226-minimum-number-game.java
@@ -3,9 +3,6 @@ class Solution {
         if(nums.length == 0) return nums;
         int i = 0;
         int j = nums.length - 1;
-        int index = 0;
-        // System.out.print(i);
-        int[] returnArr = new int[nums.length];
         while( i <= j){
            
             int alice = -1;
@@ -43,26 +40,6 @@ class Solution {
                 nums[i + 1] = nums[alice];
                 nums[alice] = placeHolder2;
             }
-
-            // // j--;
-            // if(i + 2 > j){
-            //     int placeholdera = nums[alice];
-            //     int placeholderb = nums[bob];
-            //     nums[i] = placeholderb;
-            //     nums[i+1] = placeholdera;
-            // } else {
-            // System.out.print(i + " " + (i+1) + "\n");
-            //  System.out.print(alice+ " " + bob+ "\n");
-            // System.out.print(nums[alice] + " " + nums[bob]+ "\n");
-            // int placeHolder1 = nums[i];
-            // nums[i] = nums[bob];
-            // nums[bob] = placeHolder1;
-
-            // int placeHolder2 = nums[i + 1];
-            // nums[i + 1] = nums[alice];
-            // nums[alice] = placeHolder2;
-            // System.out.print(nums[i] + " " + nums[i+1] + "\n\n"); 
-            // }
 
 
             i += 2; 

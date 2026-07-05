77bfd5d97773656596789c0beb2881d461e455fe
Daniel Sakyi <81488150+dan1306@users.noreply.github.com>
2026-07-05 09:48:58 -0400

    2752-SUM-MULTIPLES Stats: Time: 3 ms (95.36%), Space: 42.2 MB (95.91%) - LeetHub

---
+++ 2752-sum-multiples/2752-sum-multiples.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOfMultiples(int n) {
+
+        int j = 0;
+
+        for(int i = 2; i <= n; i++ ){
+            if(i % 3  == 0 || i % 5 == 0 || i % 7 == 0) j+= i;
+        } 
+
+        return j;
+        
+    }
+}

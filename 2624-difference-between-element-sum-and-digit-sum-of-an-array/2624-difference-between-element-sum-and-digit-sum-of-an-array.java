8d3a072b72712c2cd552e7c0df02b3bdb536cddb
Daniel Sakyi <81488150+dan1306@users.noreply.github.com>
2026-07-09 15:52:48 -0400

    2624-DIFFERENCE-BETWEEN-ELEMENT-SUM-AND-DIGIT-SUM-OF-AN-ARRAY Stats: Time: 2 ms (99.13%), Space: 46.2 MB (85.25%) - LeetHub

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/2624-difference-between-element-sum-and-digit-sum-of-an-array.java
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/2624-difference-between-element-sum-and-digit-sum-of-an-array.java
@@ -1,19 +1,20 @@
 class Solution {
     public int differenceOfSum(int[] nums) {
-
-        int sums = 0;
+int sums = 0;
         int digitSum = 0;
 
-        for(int i = 0; i < nums.length; i++){
-            int val = nums[i];
-            sums+= val;
-            while (val > 0) {
-                digitSum += (val % 10);
-                val /= 10;
+        // 1. Enhanced loop cuts index tracking arithmetic
+        for (int num : nums) {
+            sums += num;
+            
+            // 2. Clear out the if/else completely to prevent branch mispredictions
+            while (num > 0) {
+                digitSum += (num % 10);
+                num /= 10;
             }
         }
-
-        return Math.abs(sums - digitSum);
         
+        // 3. No condition checks or Math.abs needed; result is always positive
+        return sums - digitSum;
     }
 }

0e8bcf4af91b2f03b583217ae8d7f2d9b5577b19
Daniel Sakyi <81488150+dan1306@users.noreply.github.com>
2026-07-26 19:33:22 -0400

    0069-SQRTX Stats: Time: 0 ms (100%), Space: 9.3 MB (12.54%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.c
@@ -0,0 +1,25 @@
+int mySqrt(int x) {
+
+        int i = 0;
+        int j = x;
+        int z = 0;
+
+        while( i <= j){
+            // System.out.println(i + " " + j);
+            int mid = (i+j) / 2;
+            long midSqred = (long) mid * mid; ;
+
+            if(midSqred > x){
+                j = mid - 1;
+            }else{
+                if(midSqred == x) return mid;
+                if(mid > z) {
+                    z = mid;
+                }
+                i = mid + 1;
+            }
+            
+        }
+        return z;
+    
+}

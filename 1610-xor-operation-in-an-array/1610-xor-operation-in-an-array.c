fe598c6617a58394bcd0c3f889cc2058596479d4
Daniel Sakyi <81488150+dan1306@users.noreply.github.com>
2026-07-31 08:23:04 -0400

    1610-XOR-OPERATION-IN-AN-ARRAY Stats: Time: 0 ms (100%), Space: 8.4 MB (79.66%) - LeetHub

--- 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.c
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.c
@@ -1,11 +1,11 @@
 int xorOperation(int n, int start) {
 
-    int nums[n];
+    // int nums[n];
     int  returnMe = 0;
 
     for(int i = 0; i < n; i++) {
-        nums[i] = start + 2 * i;
-        returnMe = returnMe ^ nums[i];
+        // nums[i] = start + 2 * i;
+        returnMe = returnMe ^ (start + 2 * i);
     }
     return returnMe;
 

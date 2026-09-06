b8bfd72f8fc6d3f0e36b896b9e8f2cd1bc16c1f8
Daniel Sakyi <81488150+dan1306@users.noreply.github.com>
2026-09-06 17:34:55 -0400

    0001-TWO-SUM Stats: Time: 7621 ms (5%), Space: 13.3 MB (36.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,27 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        if nums == []:
-            return []
-        
-        if len(nums) == 1:
-            return[]
 
-        dummyList = sorted(nums);
+        # set_ = set()
 
-        l_index = 0;
-        r_index = len(nums) - 1;
+        for i in range(0, len(nums)):
+            for j in range(i + 1, len(nums)):
+                min_ = min(i, j)
+                max_ = max(i, j)
 
-        while dummyList[l_index] + dummyList[r_index] != target and l_index != r_index:
+                # if (min_, max_) in set_:
+                #     continue
 
-            if dummyList[l_index] + dummyList[r_index] > target:
-                r_index -= 1;
-            elif dummyList[l_index] + dummyList[r_index] < target:
-                l_index += 1;
-
-        if l_index != r_index and dummyList[l_index] * 2 == target:
-            placeholder = nums.index(dummyList[l_index]);
-            nums[placeholder] = "i am just here"
-            return [placeholder, nums.index(dummyList[r_index])];
-        return [nums.index(dummyList[l_index]), nums.index(dummyList[r_index])];
-        
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                # set_.add((min_, max_))
